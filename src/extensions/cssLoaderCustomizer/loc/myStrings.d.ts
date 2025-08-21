e31d7666aac74ed5e1cb4c622d2271711b59c9ad
a8pkyzz <a8pkyzz@mmm.com>
2025-08-21 10:10:29 -0600

    Initial Version

---
+++ src/extensions/cssLoaderCustomizer/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ICssLoaderCustomizerApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'CssLoaderCustomizerApplicationCustomizerStrings' {
+  const strings: ICssLoaderCustomizerApplicationCustomizerStrings;
+  export = strings;
+}
